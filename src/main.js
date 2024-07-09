a3a7147a077b10c53151460691deb87a22933216
Victor Porto Saar <victorpsaar@gmail.com>
2024-07-09 08:46:43 -0300

    Criação do footer e faq

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(btn) {
             console.log(btn);
         })
     }
+
+    for (let i = 0;i < questions.length; i++) {
+        questions[i].addEventListener('click', toggler)
+    }
 })
+
+function toggler(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const element = elemento.target.parentNode;
+
+    element.classList.toggle(classe);
+}
